d1035c4bd0ce781a68d4a376dc809ba7927da80e
Ana Rita Matos <daxtri@hotmail.com>
2017-04-27 15:20:09 +0100

    PChar

---
+++ GroupWork_DarkThrone-master/Dark_Throne_5.6/UnityDarkThrone/Assets/Scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public float speed = 1f;
+    private float health = 100f;
+
+	// Use this for initialization  
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerMove();
+	}
+
+    void playerMove()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+    }
+
+    public void TakeDamage(float dd)
+    {
+        health -= dd;
+        
+    }
+}
+
+ 
