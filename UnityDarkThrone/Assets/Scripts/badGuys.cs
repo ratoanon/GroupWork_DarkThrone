e5307b39f44062e0b3d5a20e76d9127414fd4613
Unknown <ratoanon@gmail.com>
2017-05-25 15:43:49 +0100

    Hp bars

--- UnityDarkThrone/Assets/Scripts/badGuys.cs
+++ UnityDarkThrone/Assets/Scripts/badGuys.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class badGuys : MonoBehaviour {
-    public float hp = 40.0f;
+    public float maxhp = 40.0f;
+    public float currenthp = 0;
+    public GameObject healthBar;
     // Use this for initialization
     void Start () {
-		
-	}
+        currenthp = maxhp;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,8 +19,10 @@ public class badGuys : MonoBehaviour {
 
         public void TakeDamage(float d)
     {
-        hp -= d;
-        if (hp <= 0.0f)
+        currenthp -= d;
+        float scaledhp = currenthp / maxhp;
+        setHp(scaledhp);
+        if (scaledhp <= 0.0f)
         {
             DieEnemy();
         }
@@ -26,6 +30,11 @@ public class badGuys : MonoBehaviour {
     void DieEnemy()
     {
         Destroy(gameObject, 1f);
+    }
+
+    public void setHp(float myHp)
+    {
+        healthBar.transform.localScale = new Vector3(myHp, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
     }
 
 
