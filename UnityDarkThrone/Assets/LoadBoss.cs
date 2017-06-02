47014babf847be5ac324a784c64db6e62b03c7b0
Ana Rita Matos <daxtri@hotmail.com>
2017-06-02 12:19:20 +0100

    lvl 2
    needs fix

---
+++ UnityDarkThrone/Assets/LoadBoss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBoss : MonoBehaviour {
+
+    public GameObject player;
+    private GameObject gate;
+
+    void OnTriggerStay (Collider door)
+    {
+            if (door.CompareTag("door"))
+            {
+                gate = door.gameObject;
+                Load();
+        }
+    } 
+
+    public void Load()
+    {
+       
+        if (player.GetComponent<playerController>().isAtacking)
+        { SceneManager.LoadScene(2); }
+    }
+}
---
+++ UnityDarkThrone/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+      public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
+
+
+
