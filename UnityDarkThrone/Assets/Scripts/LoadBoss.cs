f7d5dd43671bf865bbc0ea7b603a7dd1bf839608
Ana Rita Matos <daxtri@hotmail.com>
2017-06-06 11:01:07 +0100

    Calibrations
    door fixed

---
+++ UnityDarkThrone/Assets/Scripts/LoadBoss.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBoss : MonoBehaviour {
+
+    public GameObject player;
+    private GameObject gate;
+
+    void OnTriggerEnter (Collider door)
+    {
+        SceneManager.LoadScene(2);
+    } 
+
+  
+}
