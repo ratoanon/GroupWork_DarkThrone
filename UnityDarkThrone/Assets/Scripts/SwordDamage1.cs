2a33e1a9b17b4fccc3625df5993d71e8c4d2dafb
Unknown <ratoanon@gmail.com>
2017-06-08 23:34:10 +0100

    jogo semi pronto?

---
+++ UnityDarkThrone/Assets/Scripts/SwordDamage1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage1 : MonoBehaviour
+{
+    public GameObject player;
+    private GameObject badguy;
+
+
+    void OnTriggerStay(Collider enemy)
+    {
+        if (enemy.CompareTag("Enemy"))
+        {
+            badguy = enemy.gameObject;
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        badGuys1 bg = badguy.GetComponent<badGuys1>();
+        if (player.GetComponent<playerController>().isAtacking)
+        {
+            bg.TakeDamage(20);
+            player.GetComponent<playerController>().isAtacking = false;
+        }
+    }
+}
---
+++ UnityDarkThrone/Assets/Scripts/SwordDamage3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage3 : MonoBehaviour
+{
+    public GameObject player;
+    private GameObject badguy;
+
+    void OnTriggerStay(Collider enemy)
+    {
+        if (enemy.CompareTag("Enemy"))
+        {
+            badguy = enemy.gameObject;
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        badGuys1 bg = badguy.GetComponent<badGuys1>();
+        if (player.GetComponent<playerController>().isAtacking2)
+        {
+            bg.TakeDamage(20);
+            player.GetComponent<playerController>().isAtacking2 = false;
+        }
+    }
+}
